144a0eac199bb375d397e273c489936d2a753cb4
Makima-SN1572 <2471654426@qq.com>
2020-11-24 12:07:25 +0800

    学号.py

---
+++ 学号.py
@@ -0,0 +1,11 @@
+#随机抽取N个学号
+import random
+#把字典中的key取出来，转化为列表
+print (students.keys())
+for key in students.key():
+    print(key)
+random.sample(students.keys(),num)
+#根据抽取的结果，打印姓名
+for xuehao in results:
+    print (students[xuehao]）
+
